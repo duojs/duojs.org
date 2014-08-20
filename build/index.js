18d89037812d3feeb159c1cc96d177eedbfd9b9c
Matthew Mueller <mattmuelle@gmail.com>
2014-08-20 00:55:57 -0700

    commit build

---
+++ build/index.js
@@ -0,0 +1,625 @@
+(function outer(modules, cache, entries){
+
+  /**
+   * Global
+   */
+
+  var global = (function(){ return this; })();
+
+  /**
+   * Require `name`.
+   *
+   * @param {String} name
+   * @param {Boolean} jumped
+   * @api public
+   */
+
+  function require(name, jumped){
+    if (cache[name]) return cache[name].exports;
+    if (modules[name]) return call(name, require);
+    throw new Error('cannot find module "' + name + '"');
+  }
+
+  /**
+   * Call module `id` and cache it.
+   *
+   * @param {Number} id
+   * @param {Function} require
+   * @return {Function}
+   * @api private
+   */
+
+  function call(id, require){
+    var m = cache[id] = { exports: {} };
+    var mod = modules[id];
+    var name = mod[2];
+    var fn = mod[0];
+
+    fn.call(m.exports, function(req){
+      var dep = modules[id][1][req];
+      return require(dep ? dep : req);
+    }, m, m.exports, outer, modules, cache, entries);
+
+    // expose as `name`.
+    if (name) cache[name] = cache[id];
+
+    return cache[id].exports;
+  }
+
+  /**
+   * Require all entries exposing them on global if needed.
+   */
+
+  for (var id in entries) {
+    if (entries[id]) {
+      global[entries[id]] = require(id);
+    } else {
+      require(id);
+    }
+  }
+
+  /**
+   * Duo flag.
+   */
+
+  require.duo = true;
+
+  /**
+   * Expose cache.
+   */
+
+  require.cache = cache;
+
+  /**
+   * Expose modules
+   */
+
+  require.modules = modules;
+
+  /**
+   * Return newest require.
+   */
+
+   return require;
+})({
+1: [function(require, module, exports) {
+
+var Highlight = require('segmentio/highlight');
+
+/**
+ * Code highlighting.
+ */
+
+new Highlight()
+  .use(require('segmentio/highlight-javascript'))
+  .use(require('segmentio/highlight-xml'))
+  .use(require('segmentio/highlight-css'))
+  .use(require('segmentio/highlight-json'))
+  .all();
+}, {"segmentio/highlight":2,"segmentio/highlight-javascript":3,"segmentio/highlight-xml":4,"segmentio/highlight-css":5,"segmentio/highlight-json":6}],
+2: [function(require, module, exports) {
+
+var escape = require('escape-html');
+
+/**
+ * Expose `Highlight`.
+ */
+
+module.exports = Highlight;
+
+/**
+ * Initialize a new `Highlight` instance.
+ */
+
+function Highlight(){
+  if (!(this instanceof Highlight)) return new Highlight();
+  this.languages = {};
+  this.prefix('Highlight-');
+}
+
+/**
+ * Use a `plugin` function.
+ *
+ * @param {Function} plugin
+ * @return {Highlight}
+ */
+
+Highlight.prototype.use = function(plugin){
+  plugin(this);
+  return this;
+};
+
+/**
+ * Get or set the highlighted class `prefix`.
+ *
+ * @param {String} prefix
+ * @return {Highlight or String}
+ */
+
+Highlight.prototype.prefix = function(prefix){
+  if (!arguments.length) return this._prefix;
+  this._prefix = prefix;
+  return this;
+}
+
+/**
+ * Define a new `language` with a `grammar`.
+ *
+ * @param {String} language
+ * @param {Object} grammar
+ * @return {Highlight}
+ */
+
+Highlight.prototype.language = function(language, grammar){
+  this.languages[language] = grammar;
+  return this;
+};
+
+/**
+ * Highlight an HTML `string` of a given `language`.
+ *
+ * @param {String} string
+ * @param {String} language
+ * @return {String}
+ */
+
+Highlight.prototype.string = function(string, language){
+  var ast = this.parse(string, language);
+  var str = this.stringify(ast);
+  return str;
+};
+
+/**
+ * Highlight an `el`, with optional `language`.
+ *
+ * @param {Element or String} el
+ * @param {String} language (optional)
+ */
+
+Highlight.prototype.element = function(el, language){
+  if ('string' == typeof el) el = document.querySelector(el);
+  var str = this.string(el.textContent, language || lang(el));
+  el.innerHTML = str;
+};
+
+/**
+ * Highlight an array of `els`, with optional `language`.
+ *
+ * @param {Array or String} els
+ * @param {String} language (optional)
+ */
+
+Highlight.prototype.elements = function(els, language){
+  if ('string' == typeof els) els = document.querySelectorAll(els);
+  for (var i = 0, el; el = els[i]; i++) this.element(el, language);
+};
+
+/**
+ * Highlight all elements in the DOM with language attributes.
+ */
+
+Highlight.prototype.all = function(){
+  this.elements(document.querySelectorAll('[data-language]'));
+  this.elements(document.querySelectorAll('[class*="language-"]'));
+  this.elements(document.querySelectorAll('[class*="lang-"]'));
+};
+
+/**
+ * Parse a `string` with a given language's `grammar`, returning an AST.
+ *
+ * @param {String} string
+ * @param {String or Object} grammar
+ * @return {Array}
+ */
+
+Highlight.prototype.parse = function(string, grammar){
+  if ('string' == typeof grammar) {
+    var lang = grammar;
+    grammar = this.languages[lang];
+    if (!grammar) throw new Error('unknown language "' + lang + '"');
+  }
+
+  if (!grammar) throw new Error('must provide a grammar');
+  if (!string) return [];
+  var ret = [string];
+
+  for (var key in grammar) {
+    var rule = grammar[key];
+    var regexp = rule.pattern || rule;
+
+    for (var i = 0; i < ret.length; i++) {
+      var str = ret[i];
+      if ('object' == typeof str) continue;
+      var m = regexp.exec(str);
+      if (!m) continue;
+
+      var contents = m[0];
+      var before = str.slice(0, m.index);
+      var after = str.slice(m.index + contents.length);
+      var args = [i, 1];
+      var token = {
+        type: key,
+        value: rule.children ? this.parse(contents, rule.children) : contents
+      };
+
+      if (before) args.push(before);
+      args.push(token);
+      if (after) args.push(after);
+      ret.splice.apply(ret, args);
+    }
+  }
+
+  return ret;
+}
+
+/**
+ * Stringify a given `ast`.
+ *
+ * @param {Array} ast
+ * @return {String}
+ */
+
+Highlight.prototype.stringify = function(ast){
+  var prefix = this.prefix();
+  var self = this;
+
+  return ast.map(function(t){
+    if ('string' == typeof t) return escape(t);
+    var type = t.type;
+    var value = 'object' == typeof t.value
+      ? self.stringify(t.value)
+      : escape(t.value);
+    return '<span class="' + prefix + type + '">' + value + '</span>';
+  }).join('');
+};
+
+/**
+ * Language class matcher.
+ */
+
+var matcher = /\blang(?:uage)?-(\w+)\b/i;
+
+/**
+ * Get the code language for a given `el`. First look for a `data-language`
+ * attribute, then a `language-*` class, then search up the DOM tree for them.
+ *
+ * @param {Element} el
+ * @return {String}
+ */
+
+function lang(el){
+  if (!el) return;
+  var m;
+  if (el.hasAttribute('data-language')) return el.getAttribute('data-language');
+  if (m = matcher.exec(el.className)) return m[1];
+  return language(el.parentNode);
+}
+}, {"escape-html":7}],
+7: [function(require, module, exports) {
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ * @api private
+ */
+
+module.exports = function(html) {
+  return String(html)
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
+
+}, {}],
+3: [function(require, module, exports) {
+
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
+
+/**
+ * Plugin to highlight Javascript code.
+ *
+ * @param {Highlight} highlight
+ */
+
+function plugin(highlight){
+  highlight
+    .language('javascript', grammar)
+    .language('js', grammar);
+}
+
+/**
+ * Grammar.
+ */
+
+var grammar = {};
+
+/**
+ * Comments.
+ */
+
+grammar.comment = /(?!\\{2})(\/\*[\w\W]*?\*\/|\/\/.*?$)/m;
+
+/**
+ * Booleans.
+ */
+
+grammar.boolean = /\b(true|false)\b/;
+
+/**
+ * Strings.
+ */
+
+grammar.string = /(("|')(\\?.)*?\2)/;
+
+/**
+ * Keywords.
+ */
+
+grammar.keyword = /\b(break|catch|continue|delete|do|else|finally|for|function|if|in|instanceof|let|new|null|return|this|self|throw|try|typeof|var|while|with|yield)\b/;
+
+/**
+ * Constants.
+ */
+
+grammar.constant = /\b(document|window|global)\b/;
+
+/**
+ * Functions.
+ *
+ * Children are set separately to maintain ordering.
+ */
+
+grammar.function = {
+  pattern: /(\w+)\(/,
+  children: {}
+};
+
+grammar.function.children.class = /\b([A-Z]\w*)\b/;
+grammar.function.children.function = /(\w+)/;
+grammar.function.children.punctuation = /\(/;
+
+/**
+ * Numbers.
+ */
+
+grammar.number = /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?|NaN|-?Infinity)\b/;
+
+/**
+ * Operators.
+ */
+
+grammar.operator = /([-+]{1,2}|!|&lt;=?|>=?|={1,3}|&lt;{1,2}|>{1,2}|(&amp;){1,2}|\|{1,2}|\?|\*|\/|\~|\^|\%)/;
+
+/**
+ * Punctuation.
+ */
+
+grammar.punctuation = /[{}[\];(),.:]/;
+}, {}],
+4: [function(require, module, exports) {
+
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
+
+/**
+ * Plugin to highlight XML code.
+ *
+ * @param {Highlight} highlight
+ */
+
+function plugin(highlight){
+  highlight
+    .language('xml', grammar)
+    .language('html', grammar);
+}
+
+/**
+ * Grammar.
+ */
+
+var grammar = {};
+
+/**
+ * Comments.
+ */
+
+grammar.comment = /<!--[\w\W]*?-->/m;
+
+/**
+ * Entities.
+ */
+
+grammar.entity = /&#?[\dA-Za-z]{1,8};/;
+
+/**
+ * Doctypes.
+ */
+
+grammar.doctype = /<!DOCTYPE.+?>/i;
+
+/**
+ * CDATA.
+ */
+
+grammar.cdata = /<!\[CDATA\[[\w\W]*?]]>/i;
+
+/**
+ * Prologs.
+ */
+
+grammar.prolog = /<\?.+?\?>/;
+
+/**
+ * Tags. Children declared separately to maintain order.
+ */
+
+var children = {
+  string: /('|")[\w\W]*?\1/,
+  punctuation: /(^<\/?|\/?>$|=)/,
+  name: /^[\w:-]+/,
+  attribute: /[\w:-]+/
+};
+
+grammar.tag = {
+  pattern: /<\/?[\w:-]+\s*(\s+[\w:-]+(=(("|')[\w\W]*\4|[^\s'">=]+))?\s*)*\/?>/,
+  children: children
+};
+}, {}],
+5: [function(require, module, exports) {
+
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
+
+/**
+ * Plugin to highlight CSS code.
+ *
+ * @param {Highlight} highlight
+ */
+
+function plugin(highlight){
+  highlight.language('css', grammar);
+}
+
+/**
+ * Grammar.
+ */
+
+var grammar = {};
+
+/**
+ * Comments.
+ */
+
+grammar.comment = /\/\*[\w\W]*?\*\//m;
+
+/**
+ * @-rules.
+ */
+
+grammar['at-rule'] = /@[\w-]+\b/;
+
+/**
+ * Selectors.
+ */
+
+grammar.selector = {
+  pattern: /[^\{\}\s][^\{\};]*?\{/,
+  children: {
+    class: /\.[-.\w]+/,
+    id: /#[-\w]+/,
+    'pseudo-element': /:(after|before|first-letter|first-line|selection)|::[-\w]+/,
+    'pseudo-class': /:[-\w]+(\(.*\))?/,
+    punctuation: /\{/
+  }
+};
+
+/**
+ * Strings.
+ */
+
+grammar.string = /("|').*?\1/;
+
+/**
+ * Functions.
+ */
+
+grammar.function = {
+  pattern: /[\w-]+\(/,
+  children: {
+    punctuation: /\(/
+  }
+};
+
+/**
+ * Properties.
+ */
+
+grammar.property = /[\w-]+(?=\s*:)/;
+
+/**
+ * Keywords.
+ */
+
+grammar.keyword = /[\b|!]important|initial|inherit|none|transparent\b/;
+
+/**
+ * Numbers.
+ */
+
+grammar.number = /(#[A-Fa-f\d]{3,8}|\b\d*\.?\d+)/;
+
+/**
+ * Operators.
+ */
+
+grammar.operator = /[-+\/*%^]/;
+
+/**
+ * Punctuation.
+ */
+
+grammar.punctuation = /[{}(),:;]/;
+
+/**
+ * Constant.
+ */
+
+grammar.constant = /(?!\d)(ch|cm|deg|dpcm|dpi|dppx|em|ex|grad|Hz|kHz|in|mm|pc|pt|px|rad|rem|s|ms|turn|vh|vmax|vmin|vw)\b/;
+
+}, {}],
+6: [function(require, module, exports) {
+
+/**
+ * Expose `json`
+ */
+
+module.exports = json;
+
+/**
+ * Add `json` as a language.
+ *
+ * @param {Highlight} highlight
+ * @api public
+ */
+
+function json(highlight){
+  highlight.language('json', json);
+}
+
+/**
+ * Booleans
+ */
+
+json.boolean = /\b(true|false)\b/;
+
+/**
+ * Strings
+ */
+
+json.string = /("(\\?.)*")/;
+
+/**
+ * Punctuation
+ */
+
+json.punctuation = /[{}[\]:]/;
+
+/**
+ * Numbers
+ */
+
+json.number = /\b-?(\d+\.?\d*([eE]-?\d+)?)\b/;
+}, {}]}, {}, {"1":""})
