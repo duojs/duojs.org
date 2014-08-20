dfd67f7cffc6c014bf0e675b449c1078bcaa5552
Matthew Mueller <mattmuelle@gmail.com>
2014-08-20 02:43:31 -0700

    added analytics

--- build/index.js
+++ build/index.js
@@ -83,6 +83,9 @@
    return require;
 })({
 1: [function(require, module, exports) {
+/**
+ * Module Dependencies
+ */
 
 var Highlight = require('segmentio/highlight');
 
@@ -96,6 +99,21 @@ new Highlight()
   .use(require('segmentio/highlight-css'))
   .use(require('segmentio/highlight-json'))
   .all();
+
+/**
+ * Analytics
+ *
+ * TODO: use segment.io ;-)
+ */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-10351690-12', 'auto');
+ga('send', 'pageview');
+
 }, {"segmentio/highlight":2,"segmentio/highlight-javascript":3,"segmentio/highlight-xml":4,"segmentio/highlight-css":5,"segmentio/highlight-json":6}],
 2: [function(require, module, exports) {
 
